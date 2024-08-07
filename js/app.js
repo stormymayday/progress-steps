4526cb54ad0ab9e17597a5eeac5ba6e0a6c4df66
stormymayday <stormymayday7@gmail.com>
2024-08-06 23:46:44 -0700

    Changed ids for progress-line, prev-btn, and next-btn. Selecting the elements in JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log(`hello`);
+const progressLine = document.getElementById("progress-line");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const circles = document.querySelectorAll(".circle");
