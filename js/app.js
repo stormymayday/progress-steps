b38877e38483ad24f1184a94d3656de708945890
stormymayday <stormymayday7@gmail.com>
2024-08-06 23:57:59 -0700

    Implemented the 'next-btn' functionality

--- js/app.js
+++ js/app.js
@@ -2,3 +2,42 @@ const progressLine = document.getElementById("progress-line");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 const circles = document.querySelectorAll(".circle");
+
+let currentActiveCircle = 1;
+
+nextBtn.addEventListener("click", () => {
+    currentActiveCircle++;
+
+    if (currentActiveCircle > circles.length) {
+        currentActiveCircle = circles.length;
+    }
+
+    // console.log(currentActiveCircle);
+    update();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentActiveCircle--;
+
+    if (currentActiveCircle < 1) {
+        currentActiveCircle = 1;
+    }
+
+    console.log(currentActiveCircle);
+    update();
+});
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        if (index < currentActiveCircle) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    const activeCircles = document.querySelectorAll(".active");
+
+    progressLine.style.width =
+        ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%";
+};
